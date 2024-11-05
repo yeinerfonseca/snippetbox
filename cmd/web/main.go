eda25afdc085b5496c3e694af165da15e53491d2
Yeiner Fonseca <yeiner.fonseca@anuvu.com>
2024-11-05 15:58:06 -0500

    feat: install and setup dependency to parse form and clean up code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,14 +8,17 @@ import (
 	"os"
 	"text/template"
 
-	_ "github.com/go-sql-driver/mysql"
 	"github.com/yeinerfonseca/snippetbox/internal/models"
+
+	"github.com/go-playground/form/v4"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
 	logger        *slog.Logger
 	snippets      *models.SnippetModel
 	templateCache map[string]*template.Template
+	formDecoder   *form.Decoder
 }
 
 func main() {
@@ -39,10 +42,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	formDecoder := form.NewDecoder()
+
 	app := &application{
 		logger:        logger,
 		snippets:      &models.SnippetModel{DB: db},
 		templateCache: templateCache,
+		formDecoder:   formDecoder,
 	}
 
 	logger.Info("starting server", "addr", *addr)
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yeinerfonseca/snippetbox
 go 1.23.2
 
 require (
+	github.com/go-playground/form/v4 v4.2.1
 	github.com/go-sql-driver/mysql v1.8.1
 	github.com/justinas/alice v1.2.0
 )
